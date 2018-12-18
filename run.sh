650cd181d01e9ecf630e1d0941757fae1733853f
el <hylee101001@gmail.com>
2018-12-18 18:46:33 -0500

    asdf1

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python3 sptam.py --dataset kitti --path /media/el/Data/DLData/KITTI/odom/dataset/sequences/00
+#python3 sptam.py --dataset kitti --path /media/el/Data/DLData/KITTI/odom/dataset/sequences/00
+python3 sptam.py --dataset kitti --path /media/el/Samsung_T5/KITTI/odom/dataset/sequences/00
