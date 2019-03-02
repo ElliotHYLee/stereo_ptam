cd42ba014e5fc86a44c32ca7469901635d927e4d
Elliot <hylee101001@gmail.com>
2019-03-02 00:05:58 -0500

    aasdf

---
+++ scratches/test.py
@@ -0,0 +1,104 @@
+# pip libs
+import cv2
+import g2o
+import os
+import sys
+import argparse
+from threading import Thread
+
+# custom libs
+from Params.params import *
+from Dataset.KITTIOdometry import *
+from Components.Camera import *
+from Components.Frame import Frame
+from Feature.ImageFeature import *
+
+def main(dataset, params):
+    cam = Camera(
+        dataset.cam.fx, dataset.cam.fy, dataset.cam.cx, dataset.cam.cy,
+        dataset.cam.width, dataset.cam.height,
+        params.frustum_near, params.frustum_far,
+        dataset.cam.baseline)
+
+    img_0l = dataset.left[0]
+    img_0r = dataset.right[0]
+
+    featurel = ImageFeature(img_0l, params)
+    featurer = ImageFeature(img_0r, params)
+    featurel.extract()
+    featurer.extract()
+
+    pose = g2o.Isometry3d()
+    left = Frame(0, pose, featurel, cam, dataset.timestamps[0], np.identity(6))
+    right = Frame(0, cam.compute_right_camera_pose(pose), featurer, cam, dataset.timestamps[0], np.identity(6))
+
+    kps_left, desps_left, idx_left = left.get_unmatched_keypoints()
+    kps_right, desps_right, idx_right = right.get_unmatched_keypoints()
+
+    matches = featurel.row_match(kps_left, desps_left, kps_right, desps_right)
+
+    px_left = np.array([kps_left[m.queryIdx].pt for m in matches])
+    px_right = np.array([kps_right[m.trainIdx].pt for m in matches])
+
+    points = cv2.triangulatePoints(
+        left.projection_matrix,
+        right.projection_matrix,
+        px_left.transpose(),
+        px_right.transpose()
+    ).transpose()
+
+    print(left.projection_matrix)
+    print(right.projection_matrix)
+    print(px_left.transpose().shape)
+
+    pc = points[:, :3] / points[:, 3:]
+    print(pc)
+
+
+
+
+if __name__ == '__main__':
+    params = ParamsKITTI()
+
+    dataset = KITTIOdometry('/media/el/Data/DLData/KITTI/odom/dataset/sequences/00')
+    main(dataset, params)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
