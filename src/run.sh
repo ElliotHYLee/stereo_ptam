f918f01e46bc83c2f72e4f3f5a9387e48bc2ec40
elcom <hylee101001@gmail.com>
2020-08-13 12:10:42 -0700

    conda

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,1 @@
-cd src/
-python3 main.py --dataset kitti --path ./KITTI/sequences/00
-
-#sudo ln -s /mnt/KITTI ~/Github/stereo_ptam/src/
-
-
-#python3 main.py --dataset euroc --path /media/el/Data/DLData/EuRoc/mh_1
+python main.py
